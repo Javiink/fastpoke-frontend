9cc521f4442ef55ddd83044983408b899b4d501e
Javiink <javihamty@gmail.com>
2024-07-24 14:39:03 +0200

    ✨ Add imageUrl pipe to concat images server to the filename

---
+++ src/app/pipes/image-url.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Pipe({
+  name: 'imageUrl',
+  standalone: true
+})
+export class ImageUrlPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return environment.imgUrl + '/' + value;
+  }
+
+}
