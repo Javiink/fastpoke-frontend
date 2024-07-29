5feab9dfadad0464438a1b2051104c10ab78c7bc
Javiink <javihamty@gmail.com>
2024-07-29 12:24:31 +0200

    ✨ Add drinks order view

--- src/app/pages/categories/drinks/drinks.component.ts
+++ src/app/pages/categories/drinks/drinks.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Drink } from '../../../models/drink';
+import { ApiService } from '../../../services/api.service';
+import { ItemCardMdComponent } from '../../../shared/cards/item-card-md/item-card-md.component';
+import { AllergensComponent } from '../../../shared/allergens/allergens.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-drinks',
   standalone: true,
-  imports: [],
+  imports: [ItemCardMdComponent, AllergensComponent, CurrencyPipe],
   templateUrl: './drinks.component.html',
   styleUrl: './drinks.component.css'
 })
 export class DrinksComponent {
+
+  protected items?: Drink[];
+
+  constructor(private apiService: ApiService) {
+    this.apiService.get<Drink[]>('/drinks').subscribe((drinks) => {
+      this.items = drinks;
+      console.log(this.items);
+    })
+  }
 
 }
