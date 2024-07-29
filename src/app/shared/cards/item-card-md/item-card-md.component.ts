e925f873aa34da71032658195f853ffe42d7beb4
Javiink <javihamty@gmail.com>
2024-07-29 12:24:11 +0200

    ✨ Add medium item card component

---
+++ src/app/shared/cards/item-card-md/item-card-md.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { ImageUrlPipe } from '../../../pipes/image-url.pipe';
+import { Size } from '../../../models/size';
+import { SizeSelectorComponent } from '../../size-selector/size-selector.component';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-item-card-md',
+  standalone: true,
+  imports: [ImageUrlPipe, SizeSelectorComponent, CurrencyPipe],
+  templateUrl: './item-card-md.component.html',
+  styles: ''
+})
+export class ItemCardMdComponent {
+  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) image: string = '';
+  @Input() description?: string;
+  @Input() allergens?: string[];
+  @Input() sizes?: Size[];
+  @Input() price?: number;
+}
