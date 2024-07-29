ba7631d58e34df1bc69747f62f85b1ac6cdf8a10
Javiink <javihamty@gmail.com>
2024-07-29 12:59:23 +0200

    ✨ Add sidedishes order view

--- src/app/pages/categories/sidedishes/sidedishes.component.ts
+++ src/app/pages/categories/sidedishes/sidedishes.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ItemCardMdComponent } from '../../../shared/cards/item-card-md/item-card-md.component';
+import { AllergensComponent } from '../../../shared/allergens/allergens.component';
+import { CurrencyPipe } from '@angular/common';
+import { Sidedish } from '../../../models/sidedishes';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-sidedishes',
   standalone: true,
-  imports: [],
+  imports: [ItemCardMdComponent, AllergensComponent, CurrencyPipe],
   templateUrl: './sidedishes.component.html',
   styleUrl: './sidedishes.component.css'
 })
 export class SidedishesComponent {
+
+  protected items?: Sidedish[];
+
+  constructor(private apiService: ApiService) {
+    this.apiService.get<Sidedish[]>('/sidedishes').subscribe((sides) => {
+      this.items = sides;
+      console.log(this.items);
+    })
+  }
 
 }
