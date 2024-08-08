876a2342dd88ddd17d68d12841e522690c7b44c3
Javiink <javihamty@gmail.com>
2024-08-08 12:52:52 +0200

    🔧 Reorganize environment file

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -2,12 +2,13 @@ export const environment = {
   production: true,
   apiUrl: '',
   imgUrl: '',
-  maxCustomSelectableIngredients: 3,
-  maxCustomTotalIngredients: {
-    base: 1,
-    sauce: 2,
-    protein: 2,
-    topping: 3,
-    sprinkle: 2,
+  customBowl: {
+    maxDistinctIngredients: {
+      base: 1,
+      sauce: 2,
+      protein: 2,
+      topping: 3,
+      sprinkle: 2,
+    }
   }
 };
