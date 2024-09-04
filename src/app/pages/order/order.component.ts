d9cc71aa53fd0df30a2d007eb9b7a9dda3348124
Javiink <javihamty@gmail.com>
2024-09-04 12:46:44 +0200

    ✨ Order service

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,4 +1,4 @@
-import { CurrencyPipe, NgClass, NgComponentOutlet } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, NgClass, NgComponentOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BowlsComponent } from '../categories/bowls/bowls.component';
@@ -8,12 +8,14 @@ import { CustomBowlComponent } from '../categories/custom-bowl/custom-bowl.compo
 import { DrinksComponent } from '../categories/drinks/drinks.component';
 import { ItemCardXlComponent } from '../../shared/cards/item-card-xl/item-card-xl.component';
 import { ModalService } from '../../services/modal.service';
+import { Observable } from 'rxjs';
+import { OrderService } from '../../services/order.service';
 
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [NgClass, NgComponentOutlet, CurrencyPipe, FontAwesomeModule, CombosComponent, BowlsComponent, SidedishesComponent, CustomBowlComponent, DrinksComponent, ItemCardXlComponent],
+  imports: [NgClass, NgComponentOutlet, CurrencyPipe, AsyncPipe, FontAwesomeModule, CombosComponent, BowlsComponent, SidedishesComponent, CustomBowlComponent, DrinksComponent, ItemCardXlComponent],
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css'],
 })
@@ -53,8 +55,11 @@ export class OrderComponent {
   ]
 
   protected activeCategory = this.categories[1];
+  orderTotal: Observable<number>;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private orderService: OrderService) {
+    this.orderTotal = orderService.getTotal();
+  }
 
   showCategory(path: string) {
     this.activeCategory = this.categories.find(e => e.path == path)!;
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { OrderItem } from '../models/order-item';
 import { CustomBowl } from '../models/custom-bowl';
+import { Size } from '../models/size';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  items: (OrderItem | CustomBowl)[] = [];
+  items: OrderItemData[] = [];
+  itemIndex: number = 0;
   takeOut: boolean = false;
-  total: number = 0;
+
+  public total$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
   constructor() { }
 
@@ -20,4 +24,44 @@ export class OrderService {
     this.takeOut = true;
   }
 
+  addItem(data: OrderItemData){
+    if (data.category == 'custom-bowl')
+      data.item.id = this.itemIndex;
+
+    if (data.size) {
+      data.price = data.size.price
+    } else {
+      const d = data.item as ((OrderItem | CustomBowl) & { price: number })
+      data.price = d.price
+    }
+
+    this.items.push(data)
+    this.itemIndex++;
+
+    this.recalculateTotal();
+
+  }
+
+  removeItem(index: number){}
+
+  recalculateTotal(){
+    let total = 0;
+
+    const orderItems = this.items;
+    orderItems.forEach(i => {
+      total = total + i.price!;
+    });
+
+    total = Math.round(total * 100)/100;
+    this.total$.next(total);
+  }
+
+  getTotal(): Observable<number> {
+    return this.total$.asObservable();
+  }
+
 }
+
+export type ItemCategory = 'bowls' | 'combos' | 'drinks' | 'sidedishes' | 'custom-bowl';
+
+export type OrderItemData = { category: ItemCategory, item: (OrderItem | CustomBowl), size?: Size, price?: number };
