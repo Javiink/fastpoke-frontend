f86e37bd1e228117c8b69a54c60901050eef933f
Javiink <javihamty@gmail.com>
2024-09-19 11:56:11 +0200

    💄 Add Nunito typeface

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -12,7 +12,7 @@ import { AsyncPipe, NgClass } from '@angular/common';
   template: `
     <div class="hidden fixed inset-0 items-center justify-center bg-black bg-opacity-70 z-50" [ngClass]="{'!flex': (visible | async) === true }" [@backgroundAnimation]="((visible | async) || ((visible | async) && hiding))">
       <div class="relative flex flex-col bg-gradient-to-br from-sky-50 to-sky-200 rounded-2xl shadow-xl p-6 w-[90%] h-[90%] transition-all duration-300 transform" [@modalAnimation]="(visible | async) && !hiding">
-        <span class="close absolute -right-2 -top-2 px-1 py-1 -pt-2 aspect-square leading-5 text-5xl font-bold bg-slate-600 text-white rounded-full border-white border-2 hover:cursor-pointer transition-all" (click)="close()">×</span>
+        <span class="close absolute -right-2 -top-2 px-1 py-1 -pt-2 aspect-square leading-6 text-5xl font-bold bg-slate-600 text-white rounded-full border-white border-2 hover:cursor-pointer transition-all" (click)="close()">×</span>
         <h1 class="mt-4 mb-4 text-4xl text-center font-bold"><i class="fpi-dish-cutlery-sm text-3xl mr-2"></i>{{(title | async)}}</h1>
         <div class="flex-grow overflow-auto">
           <app-order-review (closeModalEvent)="close()"></app-order-review>
