e2429978232dcd4027b61d287f7225df2548efac
Javiink <javihamty@gmail.com>
2024-09-19 11:25:21 +0200

    ✨ Add routed navigation

--- src/app/pages/entrypoint/entrypoint.component.ts
+++ src/app/pages/entrypoint/entrypoint.component.ts
@@ -3,6 +3,7 @@ import { ButtonIconTextXlComponent } from '../../shared/buttons/button-icon-text
 import { LogoComponent } from '../../shared/logo/logo.component';
 import { LangSwitcherComponent } from '../../shared/lang-switcher/lang-switcher.component';
 import { OrderService } from '../../services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entrypoint',
@@ -13,14 +14,16 @@ import { OrderService } from '../../services/order.service';
 })
 export class EntrypointComponent {
 
-  constructor(private orderService: OrderService){}
+  constructor(private orderService: OrderService, private router: Router){}
 
   eatin(){
-    this.orderService.setEatin()
+    this.orderService.setEatin();
+    this.router.navigate(['order']);
   }
 
   takeout(){
-    this.orderService.setTakeout()
+    this.orderService.setTakeout();
+    this.router.navigate(['order']);
   }
 
 }
